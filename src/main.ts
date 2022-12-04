c6bf70a0504bb84cfe8250c3dddfffa234002ec8
Fabian Siatama <fas0980@gmail.com>
2022-12-04 14:46:21 -0500

    add class-validator

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // binds ValidationPipe to the entire application
+  app.useGlobalPipes(new ValidationPipe());
+
+  // apply transform to all responses
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  const config = new DocumentBuilder()
+    .setTitle('API')
+    .setDescription('SICEX AUTH API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(3000);
 }
 bootstrap();
